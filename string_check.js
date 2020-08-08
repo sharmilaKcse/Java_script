11ab389b9ef4b2a0b715bc012f8ddcef8cf652e2
sharmilaKcse <sharmiipavii@gmail.com>
2020-08-08 00:41:39 -0700

    string prgm added

---
+++ string_check.js
@@ -0,0 +1,18 @@
+let str1="program";
+let str2="script";
+    var c=0
+    var x=[]
+    for(let i=0;i<str1.length;i++){
+        for(let j=0;j<str2.length;j++){
+            if(str1[i]===str2[j]){
+              c=c+1
+            }
+        }
+        while(c===0){
+            x.push(str1[i])
+            break
+        }
+
+        c=0
+    }console.log("Char not in str2: " +x.join(","))
+
