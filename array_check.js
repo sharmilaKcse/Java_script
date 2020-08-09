ebed31378b4479ee0af41781de0fad37f8f781c0
sharmilaKcse <66429293+sharmilaKcse@users.noreply.github.com>
2020-08-09 01:54:41 -0700

    Update array_check.js

--- array_check.js
+++ array_check.js
@@ -1,6 +1,6 @@
 let array=[2,4,5,6]
-var min= Math.min(...array)
-var max= Math.max(...array)
+var min= Math.min(null,array)
+var max= Math.max(null,array)
 for(let i=1;i<=6;i++){
    if(array.includes(i)===false){
       console.log(i)
